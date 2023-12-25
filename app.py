ee5d218827231d6cca81667deb17e00b2408a070
tu0315 <takurou.usuki@gmail.com>
2023-12-25 18:59:20 +0900

    section6

--- app.py
+++ app.py
@@ -1,31 +1,33 @@
-from flask import Flask
+from flask import Flask  # Flaskをインポート
 
-app = Flask(__name__)  # アプリケーション立ち上げのインスタンス(__name__は、テンプレート読み込みの時などに内部で使用)
+# アプリケーションを立ち上げるインスタンス、テンプレート読み込みのときなどに使用する
+app = Flask(__name__)  # ルーティングで必要な変数。Flaskの中心となる。nameで場所を伝えてる？？？よくわかんね
 
 
-@app.route("/")  # デコレータ関数(後続の関数に処理を追加)
+@app.route("/")  # /にアクセス（デコレータ関数）
 def index():  # index関数
-    """ルートURLへのアクセス"""
-    return "<h1>usuki</h1>"
+    return "<h1>usuki</h1>"  # 値を返す
 
 
-@app.route("/hello")
-def hello():
-    """/helloに対応"""
-    return "<h1>hello,world</h1>"
+@app.route("/hello")  # /helloにアクセス
+def hello():  # hello関数
+    return "<h1>hello,world!!</h1>"  # 値を返す
 
 
+# 辞書型の変数posts
 posts = {
     1: "POST1",
     2: "POST2",
 }
 
 
-@app.route("/post/<int:post_id>/<post_name>")
-def show_post(post_id, post_name):
-    """特定の投稿を表示 post_id:整数値"""
-    post = posts.get(post_id)
-    return f"{post_id}: {post_name}"
+# 変数でルーティング先を動的に変更する、型定義も可能
+@app.route("/post/<int:post_id>/<post_name>")  # post/<整数値:post_id>/<post_name>にアクセス
+def show_post(post_id, post_name):  # show_post変数。引数にpost_id, post_name
+    # post = posts.get(post_id) # getで取得
+    # post = posts[post_id]  # 鉤括弧で取得
+    # return f"{post}"  # postsから取得
+    return f"{post_id}: {post_name}"  # 引数を返す
 
 
 @app.route("/user/<string:user_name>/<int:user_number>")
@@ -34,5 +36,7 @@ def show_user(user_name, user_number):
     return f"<h1>{user_name_number}</h1>"
 
 
-if __name__ == "__main__":
-    app.run(debug=True)  # debugありで実行
+if __name__ == "__main__":  # nameには__main__が入っている
+    # 本番環境では環境変数を設定するのが一般的（export FLASK_APP=XX.py）
+    app.run(debug=True)  # アプリケーションを立ち上げるためのメソッド。debugありで実行
+    # アクセスログにGETしたURLが表示されるよ
