87545ae48554e8f5ea908c2216a55ecd44bde0e8
tu0315 <takurou.usuki@gmail.com>
2024-01-06 13:45:40 +0900

    エラーハンドラー周りの処理

--- 02_template/app.py
+++ 02_template/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, abort
 
 # 自分で作ったフィルター用.pyファイルをインポート
 from custom_filters import reverse, birth_year
@@ -34,8 +34,8 @@ class UserInfo:
 def home(user_name, age, hobby):
     # インスタンス化
     user_info = UserInfo(user_name, age, hobby)
-    # user_info.greet()
     return render_template("home.html", user_info=user_info)
+    # return render_template("home.html")
 
 
 @app.route("/userlist")
@@ -46,6 +46,33 @@ def userlist():
     is_login = False
     # render_templateの引数にセットする
     return render_template("userlist.html", users=users, is_login=is_login)
+
+
+# リダイレクト実践
+@app.route("/user/<string:user_name>/<int:age>/<string:hobby>")
+def user(user_name, age, hobby):
+    if user_name in ["taro", "jiro", "takuro"]:
+        # リダイレクトする
+        return redirect(url_for("home", user_name=user_name, age=age, hobby=hobby))
+    else:
+        abort(500, "そのユーザーはリダイレクトできません。")
+
+
+# 404エラー時に発動するエラーハンドラー
+@app.errorhandler(404)
+def page_not_found(error):
+    # テキトーなURLを入力すると再現可能
+    return render_template("not_found.html"), 404
+
+
+# 500エラー時に発動するエラーハンドラー
+@app.errorhandler(500)
+def system_error(error):
+    error_description = error.description
+    return (
+        render_template("system_error.html", error_description=error_description),
+        500,
+    )
 
 
 if __name__ == "__main__":
